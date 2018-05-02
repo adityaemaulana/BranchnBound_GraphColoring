f783bfaa93df16ae8c33f515607156e43df29751
Aditya Eka Maulana <adityaemaulana@gmail.com>
2018-05-02 21:17:41 +0700

    Add description
    Adding Initial State and Final State for all function

--- bnb_mGraphColoring.cpp
+++ bnb_mGraphColoring.cpp
@@ -6,7 +6,12 @@
 
 void printSolution(int color[], int V);
 
-/* Fungsi untuk pengecekan apakah vertex v dapat diberikan warna*/
+/*
+    IS: Diterima matriks dari status ketetanggaan graf, larik warna dari setiap vertex,
+        warna yang akan dimasukkan, dan jumlah Vertex pada graf
+    FS: Mengembalikan true apabila vertex dapat diisi dengan warna c dan false untuk sebaliknya
+
+*/
 bool isSafe (int v, bool graph[][M], int color[], int c, int V)
 {
     for (int i = 0; i < V; i++)
@@ -15,7 +20,12 @@ bool isSafe (int v, bool graph[][M], int color[], int c, int V)
     return true;
 }
 
-/* Fungsi untuk menghitung jumlah tetangga dari vertex v*/
+/*
+    IS: Diterima matriks dari status ketetanggaan graf, index vertex yang akan dicek,
+        indeks awal dari vertex tetangga, dan jumlah Vertex pada graf
+    FS: Mengembalikan jumlah tetangga dari vertex ke-v
+
+*/
 int countAdjacent(bool graph[][M], int v, int i, int V)
 {
     if(i == V)
@@ -28,25 +38,37 @@ int countAdjacent(bool graph[][M], int v, int i, int V)
     }
 }
 
-/* Fungsi untuk menentukan vertex yang memiliki tetangga paling banyak dan belum diberi warna */
+/*
+    IS: Diterima matriks dari status ketetanggaan graf, larik warna pada seluruh vertex,
+        dan jumlah Vertex pada graf
+    FS: Mengembalikan index vertex yang belum diwarnai dan memiliki tetangga paling banyak
+
+*/
 int maxAdjacent (bool graph[][M], int color[], int V)
 {
-    int m = 0;int midx = 0;
+    int v = 0; int vidx = 0;
+
     for(int i = 0; i < V; i++){
         if(color[i] == 0){
            int temp = countAdjacent(graph, i, 0, V);
-           if(m < temp){
-              m = temp;
-              midx = i;
+           if(v < temp){
+              v = temp;
+              vidx = i;
            }
         }
 
     }
 
-    return midx;
+    return vidx;
 }
 
-/* Fungsi recursive untuk penyelesaian masalah m-coloring graph*/
+/*
+    IS: Diterima matriks dari status ketetanggaan graf, jumlah warna yang tersedia,
+        larik warna pada seluruh vertex, dan jumlah Vertex pada graf
+    FS: Mengembalikan true apabila seluruh vertex telah diwarnai, dan mengembalikan false
+        apabila ada vertex yang tidak dapat diwarnai atau warna yang tersedia telah habis
+
+*/
 bool graphColoringUtil(bool graph[][M], int m, int color[], int colored, int V)
 {
     /* base case: Jika semua vertex telah diberi warna maka akan return true*/
@@ -76,11 +98,17 @@ bool graphColoringUtil(bool graph[][M], int m, int color[], int colored, int V)
     return false;
 }
 
+/*
+    IS: 1.Diterima matriks dari status ketetanggaan graf, jumlah warna yang tersedia,
+        dan jumlah Vertex pada graf
+        2. Larik warna untuk setiap vertex di inisialisasi dengan nilai 0
+    FS: Menampilkan seluruh elemen pada larik warna sebagai solusi dari permasalahan program
 
+*/
 bool graphColoring(bool graph[][M], int m, int V)
 {
     // Menginisialisasi seluruh warna dengan 0
-    int *color = new int[V];
+    int *color = new int[V]; //inisialisasi array dari warna setiap vertex
     for (int i = 0; i < V; i++)
        color[i] = 0;
 
@@ -96,7 +124,11 @@ bool graphColoring(bool graph[][M], int m, int V)
     return true;
 }
 
-/* Fungsi untuk menampilkan solusi setelah graph diberi warna */
+/*
+    IS: Diterima larik warna yang sudah diisi dan jumlah vertex pada graf
+    FS: Menampilkan seluruh isi larik warna
+
+*/
 void printSolution(int color[], int V)
 {
     printf("\nSolution Exists:"
@@ -108,22 +140,29 @@ void printSolution(int color[], int V)
 
 int main()
 {
-    int V, m;
-    /* Create following graph and test whether it is 3 colorable
+    int V, m; //V adalah jumlah vertex dan m adalah jumlah warna yang tersedia
+
+    /*
+    ==>apabila program dijalankan tanpa menerima input
+
+    Create following graph and test whether it is 3 colorable
       (3)---(2)
        |   / |
        |  /  |
        | /   |
       (0)---(1)
     */
-   /* bool graph[V][V] = {
+    /*bool graph[V][100] = {
         {0, 1, 1, 0, 0},
         {1, 0, 1, 1, 0},
         {1, 1, 0, 1, 1},
         {0, 1, 1, 0, 1},
         {0, 0, 1, 1, 0}
     };
-    int m = 4; // Number of colors*/
+    int m = 4; // Number of colors
+
+    */
+
 
     //masukkan jumlah vertex yang diinginkan
     printf("Jumlah vertex: ");
@@ -146,7 +185,7 @@ int main()
     printf("\n\nJumlah warna yang tersedia: ");
     scanf("%d", &m);
 
-    graphColoring (graph, m, V);
+    graphColoring(graph, m, V);
 
     return 0;
 }
